d9d30124d35d14ce51c4ea25c3e4d1e968acf5b8
MO <1192940849@qq.com>
2022-03-16 21:49:23 +0800

    接通node、mysql与vue的数据

---
+++ src/views/home/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    我是主页
+    <span>123</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  created () {
+    this.$http.get(`/user`).then(res => {
+      console.log('请求数据')
+      console.log(res)
+    })
+  }
+}
+</script>
+
+<style scoped lang="scss">
+div{
+  color: red;
+  span{
+    color: aquamarine;
+  }
+}
+</style>
