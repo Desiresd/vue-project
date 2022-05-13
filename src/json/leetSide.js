df03fc8ec7f262e643670ae4984caadf1b8dfe4c
MO <1192940849@qq.com>
2022-05-13 11:27:06 +0800

    整理路由与侧边栏数据

---
+++ src/json/leetSide.js
@@ -0,0 +1,49 @@
+// 存放leetCode题目的侧边栏数据
+
+export const leetSide = [
+  {
+    name: '两数相加求目标值',
+    route: '/leetCode/twoSum',
+    diff: '1'
+  },
+  {
+    name: '两个数组的交集',
+    route: '/leetCode/interSection',
+    diff: '1'
+  },
+  {
+    name: '两数相加（链表）',
+    route: '/leetCode/addTwoNumbers',
+    diff: '2'
+  },
+  {
+    name: '两两交换链表中的节点（链表）',
+    route: '/leetCode/swapPairs',
+    diff: '2'
+  },
+  {
+    name: '反转链表（链表）',
+    route: '/leetCode/reverseList',
+    diff: '2'
+  },
+  {
+    name: '旋转字符串',
+    route: '/leetCode/rotateString',
+    diff: '1'
+  },
+  {
+    name: '重复的DNA序列',
+    route: '/leetCode/findRepeatedDnaSequences',
+    diff: '2'
+  },
+  {
+    name: '无重复字符的最长子串',
+    route: '/leetCode/lengthOfLongestSubstring',
+    diff: '2'
+  },
+  {
+    name: '检查是否是回文数',
+    route: '/leetCode/isPalindrome',
+    diff: '2'
+  }
+]
--- src/views/leetCode/index.vue
+++ src/views/leetCode/index.vue
@@ -26,63 +26,12 @@
 </template>
 
 <script>
-import { twoSum } from '@/common/leetCode/twoSum.js'
-// import twoSum from './twoSum.vue'
+import { leetSide } from '@/json/leetSide.js'
 export default {
   name: 'index',
-  components: {
-    twoSum
-  },
   data () {
-    const sideData = [
-      {
-        name: '两数相加求目标值',
-        route: '/leetCode/twoSum',
-        diff: '1'
-      },
-      {
-        name: '两个数组的交集',
-        route: '/leetCode/interSection',
-        diff: '1'
-      },
-      {
-        name: '两数相加（链表）',
-        route: '/leetCode/addTwoNumbers',
-        diff: '2'
-      },
-      {
-        name: '两两交换链表中的节点（链表）',
-        route: '/leetCode/swapPairs',
-        diff: '2'
-      },
-      {
-        name: '反转链表（链表）',
-        route: '/leetCode/reverseList',
-        diff: '2'
-      },
-      {
-        name: '旋转字符串',
-        route: '/leetCode/rotateString',
-        diff: '1'
-      },
-      {
-        name: '重复的DNA序列',
-        route: '/leetCode/findRepeatedDnaSequences',
-        diff: '2'
-      },
-      {
-        name: '无重复字符的最长子串',
-        route: '/leetCode/lengthOfLongestSubstring',
-        diff: '2'
-      },
-      {
-        name: '检查是否是回文数',
-        route: '/leetCode/isPalindrome',
-        diff: '2'
-      }
-    ]
     return {
-      sideData: sideData
+      sideData: leetSide
     }
   }
 }
