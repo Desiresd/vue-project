3291d9430897954111cb9444053f88d4173d6e2c
MO <1192940849@qq.com>
2022-05-25 16:18:44 +0800

    表格封装

--- src/components/lis/LisTable.vue
+++ src/components/lis/LisTable.vue
@@ -2,30 +2,167 @@
   <div>
     <a-table bordered
              class="table"
+             size="middle"
              :rowKey="(record,index)=>{ return index }"
              :data-source="tableData"
              :columns="columns"
-             size="middle"
              :pagination="false"
              :loading="tableLoad"
              :scroll="scroll">
-      <slot></slot>
+      <!-- lisInput -->
+      <template slot="input"
+                slot-scope="text,record,index,column">
+        <lis-input v-if="record.editable"
+                   v-model="record[column.dataIndex]"
+                   placeholder="请输入" />
+        <lis-ellipsis-tip v-else
+                          :value="text"
+                          :length="column.length || 10" />
+      </template>
+      <!-- LisDict -->
+      <template slot="select"
+                slot-scope="text,record,index,column">
+        <lis-dict v-if="record.editable"
+                  v-model="record[column.dataIndex]"
+                  type="select"
+                  dictCode="sex" />
+        <lis-dict v-else
+                  :value="text"
+                  dictCode="sex" />
+      </template>
+      <!-- lisInputNumber -->
+      <template slot="inputNumber"
+                slot-scope="text,record,index,column">
+        <lis-input-number v-if="record.editable"
+                          v-model="record[column.dataIndex]"
+                          :formatter="column.formatter ? value => formatter(value) : null" />
+        <span v-else>{{ column.formatter ? formatter(text) : text }}</span>
+      </template>
+      <!-- LisTextArea -->
+      <template slot="textArea"
+                slot-scope="text,record,index,column">
+        <lis-text-area v-if="record.editable"
+                       v-model="record[column.dataIndex]" />
+        <lis-ellipsis-tip v-else
+                          :value="text"
+                          :length="column.length || 10" />
+      </template>
+      <!-- LisDate -->
+      <template slot="date"
+                slot-scope="text,record,index,column">
+        <lis-date v-if="record.editable"
+                  v-model="record[column.dataIndex]"
+                  allowClear
+                  style="width: 100%" />
+        <span v-else>{{  text }}</span>
+      </template>
+      <!-- LisDate -->
+      <template slot="dateArea"
+                slot-scope="text,record,index,column">
+        <lis-date-area v-if="record.editable"
+                       v-model="record[column.dataIndex]"
+                       :end.sync="record[column.dataIndexT]"
+                       :startName="column.dataIndex"
+                       :endName="column.dataIndexT"
+                       allowClear />
+        <span v-else>{{ `${text} ~ ${record[column.dataIndexT]}`  }}</span>
+      </template>
+      <!-- dateYear -->
+      <template slot="dateYear"
+                slot-scope="text,record,index,column">
+        <lis-date-year v-if="record.editable"
+                       v-model="record[column.dataIndex]"
+                       style="width: 100%" />
+        <span v-else>{{  text }}</span>
+      </template>
+      <!-- Action -->
+      <template slot="action"
+                slot-scope="text,record,index">
+        <div v-if="record.editable">
+          <a @click="save(record,index)">保存</a>
+          <a-divider type="vertical" />
+          <a-popconfirm placement="topRight"
+                        ok-text="确定"
+                        cancel-text="取消"
+                        @confirm="cancel(record,index)">
+            <template slot="title">
+              <p>确定取消保存吗?</p>
+            </template>
+            <a class="red">取消</a>
+          </a-popconfirm>
+        </div>
+        <div v-else>
+          <a @click="edit(index)">编辑</a>
+          <a-divider type="vertical" />
+          <a-popconfirm placement="topRight"
+                        ok-text="确定"
+                        cancel-text="取消"
+                        @confirm="del(record,index)">
+            <template slot="title">
+              <p>确定要删除吗?</p>
+            </template>
+            <a class="red">删除</a>
+          </a-popconfirm>
+        </div>
+      </template>
     </a-table>
   </div>
 </template>
 
 <script>
+import LisEllipsisTip from '@/components/lis/LisEllipsisTip'
+import LisDict from '@/components/lis/LisDict'
+import LisInput from '@/components/lis/LisInput'
+import LisInputNumber from '@/components/lis/LisInputNumber'
+import LisTextArea from '@/components/lis/LisTextArea'
+import LisDate from '@/components/lis/LisDate'
+import LisDateArea from '@/components/lis/LisDateArea'
+import LisDateYear from '@/components/lis/LisDateYear'
 export default {
   name: 'LisTable',
+  components: {
+    LisEllipsisTip,
+    LisDict,
+    LisInput,
+    LisInputNumber,
+    LisTextArea,
+    LisDate,
+    LisDateArea,
+    LisDateYear
+  },
   props: {
-    tableData: Array,
+    value: Array,
     columns: Array,
     scroll: Object
   },
+  watch: {
+    value: {
+      handler (newName) {
+        console.log(newName)
+        this.tableData = JSON.parse(JSON.stringify(newName))
+      },
+      immediate: true
+    }
+  },
   data () {
     return {
-      tableLoad: false
+      tableLoad: false,
+      tableData: []
     }
+  },
+  methods: {
+    formatter (text) {
+      return `${text}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    },
+    save () { },
+    cancel () { },
+    edit (index) {
+      this.tableLoad = true
+      this.tableData[index]['editable'] = true
+      this.tableLoad = false
+      console.log(this.tableData)
+    },
+    del () { }
   }
 }
 </script>
@@ -34,5 +171,9 @@ export default {
 .table {
   position: relative;
   padding: 10px;
+}
+
+.red {
+  color: red;
 }
 </style>
--- src/views/lis/LisTable.vue
+++ src/views/lis/LisTable.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <lis-table :columns="columns"
-               :tableData="tableData"
+               :value="tableData"
                :scroll="{x: 1200}">
 
     </lis-table>
@@ -19,66 +19,93 @@ export default {
     return {
       columns: [
         {
-          title: '类型',
-          dataIndex: 'contractPackageType',
+          title: '姓名',
+          dataIndex: 'name',
           width: '200px',
           align: 'center',
-          scopedSlots: { customRender: 'contractPackageType' }
+          scopedSlots: { customRender: 'input' }
         },
         {
-          title: '编号',
-          dataIndex: 'contractPackageNo',
+          title: '性别',
+          dataIndex: 'sex',
           width: '200px',
           align: 'center',
-          scopedSlots: { customRender: 'contractPackageNo' }
+          scopedSlots: { customRender: 'select' }
         },
         {
-          title: '概述',
-          dataIndex: 'contractPackageRemark',
+          title: '年龄',
+          dataIndex: 'age',
           width: '200px',
           align: 'center',
-          scopedSlots: { customRender: 'contractPackageRemark' }
+          scopedSlots: { customRender: 'inputNumber' }
         },
         {
-          title: '预计金额(元)',
-          dataIndex: 'estimatedTotalInvest',
+          title: '存款(元)',
+          dataIndex: 'money',
           width: '200px',
           align: 'center',
-          scopedSlots: { customRender: 'estimatedTotalInvest' }
+          formatter: true,
+          scopedSlots: { customRender: 'inputNumber' }
         },
         {
-          title: '采购方式',
-          dataIndex: 'purchasingMethod',
+          title: '时间(单)',
+          dataIndex: 'date',
           width: '200px',
           align: 'center',
-          scopedSlots: { customRender: 'purchasingMethod' }
+          scopedSlots: { customRender: 'date' }
         },
         {
-          title: '公告时间',
-          dataIndex: 'planYear',
+          title: '时间(双)',
+          dataIndex: 'dateStart',
+          dataIndexT: 'dateEnd',
+          width: '350px',
+          align: 'center',
+          scopedSlots: { customRender: 'dateArea' }
+        },
+        {
+          title: '时间(年)',
+          dataIndex: 'dateYear',
           width: '200px',
           align: 'center',
-          scopedSlots: { customRender: 'planYear' }
+          scopedSlots: { customRender: 'dateYear' }
         },
         {
           title: '备注',
           dataIndex: 'remark',
           width: '200px',
           align: 'center',
-          ellipsis: true,
-          scopedSlots: { customRender: 'remark' }
+          scopedSlots: { customRender: 'textArea' }
         },
         {
           title: '操作',
-          width: 200,
+          width: 150,
           align: 'center',
-          scopedSlots: { customRender: 'action' },
-          fixed: 'right'
+          fixed: 'right',
+          scopedSlots: { customRender: 'action' }
         }
       ],
       tableData: [
         {
-
+          name: 'mo1',
+          sex: '0',
+          age: '18',
+          money: 10000,
+          date: '2022-05-01',
+          dateStart: '2022-05-01',
+          dateEnd: '2022-05-21',
+          dateYear: '2022',
+          remark: '我是备注我是备注我是备注我是备注'
+        },
+        {
+          name: 'mo2',
+          sex: '1',
+          age: '22',
+          money: 80000,
+          date: '2022-05-01',
+          dateStart: '2022-05-01',
+          dateEnd: '2022-05-22',
+          dateYear: '2023',
+          remark: '我是备注我是备注我是备注我是备注'
         }
       ]
     }
