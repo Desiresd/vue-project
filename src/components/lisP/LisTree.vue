6a6365b1969cc9e56ab15e0f307efbf6822c1ad2
MO <1192940849@qq.com>
2022-11-22 15:35:12 +0800

    添加树形组件

--- src/components/lisP/LisTree.vue
+++ src/components/lisP/LisTree.vue
@@ -1,15 +1,44 @@
 <template>
   <div>
-
+    <a-tree :tree-data="treeData"
+            :show-icon="showIcon"
+            :replaceFields="replaceFields"
+            :checkable="checkable"
+            :checkedKeys="checkedKeys"
+            @select="onSelect"
+            @check="onCheck">
+      <a-icon slot="project"
+              type="file-search" />
+      <a-icon slot="subProject"
+              type="snippets" />
+    </a-tree>
   </div>
 </template>
 
 <script>
 export default {
   name: 'LisTree',
+  props: {
+    treeData: Array,
+    showIcon: Boolean,
+    replaceFields: Object,
+    checkable: {
+      type: Boolean,
+      default: () => { return false }
+    },
+    checkedKeys: Array
+  },
   data () {
     return {
 
+    }
+  },
+  methods: {
+    onSelect (key, event) {
+      this.$emit('onSelect', key, event)
+    },
+    onCheck (key, event) {
+      this.$emit('onCheck', key, event)
     }
   }
 }
--- src/views/home/lisP/LisTree.vue
+++ src/views/home/lisP/LisTree.vue
@@ -1,23 +1,133 @@
 <template>
   <div>
-
+    <lis-row>
+      <lis-col :span="12">
+        <lis-tree class="con"
+                  :treeData="treeData"
+                  :showIcon="true"
+                  :replaceFields="{'key': 'no', 'title':'name'}"
+                  :checkable="true"
+                  :checkedKeys="checkedKeys"
+                  @onSelect="onSelect"
+                  @onCheck="onCheck">
+        </lis-tree>
+      </lis-col>
+      <lis-col :span="12">
+        <lis-tree class="con"
+                  :treeData="treeData"
+                  :showIcon="true"
+                  :replaceFields="{'key': 'no', 'title':'name'}"
+                  @onSelect="onSelect"
+                  @onCheck="onCheck">
+        </lis-tree>
+      </lis-col>
+    </lis-row>
+    <high-light>{{ msg }}</high-light>
   </div>
 </template>
 
 <script>
-import Listree from '@/components/lisP/LisTree'
+import HighLight from '@/components/lisP/HighLight'
+import LisRow from '@/components/lisP/LisRow'
+import LisCol from '@/components/lisP/LisCol'
+import LisTree from '@/components/lisP/LisTree'
 export default {
   name: 'LisTrees',
   components: {
-    Listree
+    HighLight,
+    LisRow,
+    LisCol,
+    LisTree
   },
   data () {
     return {
+      msg: '',
+      treeData: [],
+      selectedKeys: [],
+      checkedKeys: []
+    }
+  },
+  created () {
+    this.msg = `<template>
+      <lis-tree class="con"
+                :treeData="treeData"
+                :showIcon="true"
+                :replaceFields="{'key': 'no', 'title':'name'}"
+                :checkable="true"
+                :checkedKeys="checkedKeys"
+                @onSelect="onSelect"
+                @onCheck="onCheck">
+      </lis-tree>
 
+      <lis-tree class="con"
+                :treeData="treeData"
+                :showIcon="true"
+                :replaceFields="{'key': 'no', 'title':'name'}"
+                @onSelect="onSelect"
+                @onCheck="onCheck">
+      </lis-tree>
+    </template>
+    
+    export default {
+      data () {
+        return {
+          treeData: [],
+          selectedKeys: [],
+          checkedKeys: []
+        }
+      },
+      created () {
+        this.treeData = [
+          {
+            name: 'parent 1',
+            no: '0-0',
+            slots: {
+              icon: 'project'
+            },
+            children: [
+              { name: 'leaf', no: '0-0-0', slots: { icon: 'subProject' } },
+              { name: 'leaf', no: '0-0-1', slots: { icon: 'subProject' } }
+            ]
+          }
+        ]
+      },
+      methods: {
+        onSelect (key, event) {
+          this.selectedKeys = key
+        },
+        onCheck (key, event) {
+          this.checkedKeys = key
+        }
+      }
+    }`
+    this.treeData = [
+      {
+        name: 'parent 1',
+        no: '0-0',
+        slots: {
+          icon: 'project'
+        },
+        children: [
+          { name: 'leaf', no: '0-0-0', slots: { icon: 'subProject' } },
+          { name: 'leaf', no: '0-0-1', slots: { icon: 'subProject' } }
+        ]
+      }
+    ]
+  },
+  methods: {
+    onSelect (key, event) {
+      this.selectedKeys = key
+    },
+    onCheck (key, event) {
+      this.checkedKeys = key
     }
   }
 }
 </script>
 
 <style scoped>
+.con {
+  width: 300px;
+  border: 1px solid #f5f5f5;
+}
 </style>
